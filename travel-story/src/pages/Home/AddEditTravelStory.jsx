3d35104de5025e89415724dfd2aade2f5bd6ea9b
Hritesh_Saha <hriteshsaha4@gmail.com>
2025-01-24 14:25:33 +0530

    add date selector

--- travel-story/src/pages/Home/AddEditTravelStory.jsx
+++ travel-story/src/pages/Home/AddEditTravelStory.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
-import { MdAdd, MdClose } from 'react-icons/md'
+import React, { useState } from 'react'
+import { MdAdd, MdClose, MdDeleteOutline, MdUpdate } from 'react-icons/md'
+import DateSelector from '../../components/input/DateSelector';
 
 const AddEditTravelStory = ({ storyInfo,
     type,
     onClose,
     getAllTravelStories,
 }) => {
+    const [title, setTitle] = useState("");
+    const [storyImg, setStoryImg] = useState(null);
+    const [story, setStory] = useState("");
+    const [visitedLocation, setVisitedLocation] = useState([]);
+    const [visitedDate, setVisitedDate] = useState(null);
+
+    const handleAddorUpdateClick = () => {};
   return (
     <div>
         <div className="flex items-center justify-between">
@@ -14,9 +22,16 @@ const AddEditTravelStory = ({ storyInfo,
             </h5>
             <div>
             <div className='flex items-center gap-3 bg-cyan-50/50 p-2 rounded-l-lg'>
-                <button className='btn-small' onClick={()=>{}}>
+                {type === "add" ? (
+                    <button className='btn-small' onClick={handleAddorUpdateClick}>
                     <MdAdd className='text-lg'/> ADD STORY
-                </button>
+                </button> ) : (
+                    <>
+                <button className='btn-small' onClick={handleAddorUpdateClick}>
+                    <MdUpdate className='text-lg'/>UPDATE STORY
+                    </button>
+
+                    </>)}
 
                 <button className='' onClick={onClose}>
                     <MdClose className='text-xl text-slate-400'/>
@@ -25,8 +40,40 @@ const AddEditTravelStory = ({ storyInfo,
         </div>
         </div>
 
+        <div>
+            <div className="flex-1 flex flex-col gap-2 pt-4">
+                <label className='input-box'>TITLE</label>
+                <input type="text"
+                className='text-2xl text-slate-950 outline-none'
+                placeholder='A Day at the Great Wall' 
+                value={title}
+                onChange={(e)=>setTitle(e.target.value)}
+                />
+
+                <div className="my-3">
+                    <DateSelector date={visitedDate} setDate={setVisitedDate} />
+                </div>
+
+                <div className='flex flex-col gap-2 mt-4'>
+                    <label className="input-label">STORY</label>
+                    <textarea 
+                    type="text"
+                    className='text-sm text-slate-950 outline-none bg-slate-50 p-2 rounded'
+                    placeholder='Your Story'
+                    rows={10}
+                    value={story}
+                    onChange={(e)=>setStory(e.target.value)}></textarea>
+                </div>
+            </div>
+        </div>
+
     </div>
   )
 }
 
 export default AddEditTravelStory
+
+
+//<button className='btn-small btn-delete' onClick={onClose}>
+//                        <MdDeleteOutline className='text-lg'/> DELETE
+//                    </button>
